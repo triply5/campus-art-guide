18aadb947bc55991aba31581a7224b85ec29ab4a
triply5 <triply5y@gmail.com>
2026-03-09 13:06:21 +0800

    Add files via upload

--- data.js
+++ data.js
@@ -2,15 +2,43 @@ const artworks = [
 
 {
 title:"青春",
-image:"https://via.placeholder.com/300x200",
+image:"青春.png",
 desc:"这幅作品表达青春与成长。",
 likes:0
 },
 
 {
 title:"自然之光",
-image:"https://via.placeholder.com/300x200",
+image:"自然之光.png",
 desc:"通过光影表现人与自然。",
+likes:0
+},
+
+{
+title:"朝阳",
+image:"朝阳.png",
+desc:"这幅作品表达了毕业学子迎来了新的开始。",
+likes:0
+},
+
+{
+title:"七彩祥云",
+image:"七彩祥云.png",
+desc:"这幅作品表达了毕业学子的未来会像七彩祥云一样美艳。",
+likes:0
+},
+
+{
+title:"森林",
+image:"森林.png",
+desc:"略",
+likes:0
+},
+
+{
+title:"热气球",
+image:"热气球.png",
+desc:"略",
 likes:0
 }
 
